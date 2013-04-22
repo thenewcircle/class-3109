c49f7639927e3eb0e0b488ebec09c10913caed08
Marko Gargenta <marko@marakana.com>
2013-04-22 15:37:19 -0700

    Implemented C++ version

--- FibNative/obj/local/mips/objs/FibNative/FibNative.o.d
+++ FibNative/obj/local/mips/objs/FibNative/FibNative.o.d
@@ -1,7 +1,4 @@
-obj/local/mips/objs/FibNative/FibNative.o: jni/FibNative.c \
- jni/com_cisco_fibnative_FibLib.h \
+obj/local/mips/objs/FibNative/FibNative.o: jni/FibNative.cpp \
  /Users/marko/tools/android-ndk-r8b/platforms/android-14/arch-mips/usr/include/jni.h
-
-jni/com_cisco_fibnative_FibLib.h:
 
 /Users/marko/tools/android-ndk-r8b/platforms/android-14/arch-mips/usr/include/jni.h:
