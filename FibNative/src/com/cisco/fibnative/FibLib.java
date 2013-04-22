ab2f7ff877dd088ae1e4d8415b906802d1d4289b
Marko Gargenta <marko@marakana.com>
2013-04-22 12:23:20 -0700

    Inital checkin

---
+++ FibNative/src/com/cisco/fibnative/FibLib.java
@@ -0,0 +1,13 @@
+package com.cisco.fibnative;
+
+public class FibLib {
+	
+	/** Java version */
+	public static long fibJ(long n) {
+		assert(n>=0);
+		if(n==0) return 0;
+		if(n==1) return 1;
+		return fibJ(n-1) + fibJ(n-2);
+	}
+
+}
