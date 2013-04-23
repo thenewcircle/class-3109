d1851e6a47e235d8a6900d31359956ca28c5a226
Marko Gargenta <marko@marakana.com>
2013-04-23 15:40:15 -0700

    Refactored to use FibManager

--- FibClient/src/com/cisco/fibclient/MainActivity.java
+++ FibClient/src/com/cisco/fibclient/MainActivity.java
@@ -1,24 +1,18 @@
 package com.cisco.fibclient;
 
 import android.app.Activity;
-import android.content.ComponentName;
-import android.content.Context;
-import android.content.Intent;
-import android.content.ServiceConnection;
 import android.os.Bundle;
-import android.os.IBinder;
-import android.os.RemoteException;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import com.cisco.fibcommon.IFibService;
+import com.cisco.fibcommon.FibManager;
 
 public class MainActivity extends Activity {
 	private EditText input;
 	private TextView output;
-	private static IFibService fibService;
+	private FibManager fibManager;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -27,36 +21,8 @@ public class MainActivity extends Activity {
 
 		input = (EditText) findViewById(R.id.input);
 		output = (TextView) findViewById(R.id.output);
-	}
-
-	private static final Intent FIB_SERVICE_ACTION = new Intent(
-			"com.cisco.fibservice.action.FIB_SERVICE");
-
-	private static final ServiceConnection CONNECTION = new ServiceConnection() {
-
-		@Override
-		public void onServiceConnected(ComponentName name, IBinder binder) {
-			fibService = IFibService.Stub.asInterface(binder);
-		}
-
-		@Override
-		public void onServiceDisconnected(ComponentName name) {
-			fibService = null;
-		}
-	};
-
-	@Override
-	protected void onStart() {
-		super.onStart();
-		// Bind to the service
-		bindService(FIB_SERVICE_ACTION, CONNECTION, Context.BIND_AUTO_CREATE);
-	}
-
-	@Override
-	protected void onStop() {
-		super.onStop();
-		// Unbind from the service
-		unbindService(CONNECTION);
+		
+		fibManager = new FibManager(this);
 	}
 
 	/** Called when the button gets clicked. */
@@ -66,20 +32,17 @@ public class MainActivity extends Activity {
 		try {
 			// Java
 			long start = System.currentTimeMillis();
-			long resultJ = fibService.fibJ(n);
+			long resultJ = fibManager.fibJ(n);
 			long timeJ = System.currentTimeMillis() - start;
 			output.append(String.format("\n fibJ(%d) = %d (%d ms)", n, resultJ,
 					timeJ));
 
 			// Native
 			start = System.currentTimeMillis();
-			long resultN = fibService.fibN(n);
+			long resultN = fibManager.fibN(n);
 			long timeN = System.currentTimeMillis() - start;
 			output.append(String.format("\n fibN(%d) = %d (%d ms)", n, resultN,
 					timeN));
-		} catch (RemoteException re) {
-			Log.e("FibNative", "Died", re);
-			re.printStackTrace();
 		} catch (Exception e) {
 			Log.e("FibNative", "Died", e);
 			e.printStackTrace();
---
+++ FibCommon/src/com/cisco/fibcommon/FibManager.java
@@ -0,0 +1,66 @@
+package com.cisco.fibcommon;
+
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
+import android.os.RemoteException;
+
+public class FibManager {
+	private Context context;
+	private static IFibService fibService;
+
+	public FibManager(Context context) {
+		super();
+		this.context = context;
+		// Bind to the service
+		context.bindService(FIB_SERVICE_ACTION, CONNECTION, Context.BIND_AUTO_CREATE);
+	}
+
+	@Override
+	public void finalize() {
+		// Unbind from the service
+		context.unbindService(CONNECTION);
+	}
+	
+	private static final Intent FIB_SERVICE_ACTION = new Intent(
+			"com.cisco.fibservice.action.FIB_SERVICE");
+
+	private static final ServiceConnection CONNECTION = new ServiceConnection() {
+
+		@Override
+		public void onServiceConnected(ComponentName name, IBinder binder) {
+			fibService = IFibService.Stub.asInterface(binder);
+		}
+
+		@Override
+		public void onServiceDisconnected(ComponentName name) {
+			fibService = null;
+		}
+	};
+
+	
+	// --- Proxy Calls ---
+	
+	public long fibJ(long n) {
+		if(fibService==null) return -1;
+		try {
+			return fibService.fibJ(n);
+		} catch (RemoteException e) {
+			e.printStackTrace();
+			return -1;
+		}
+	}
+	
+	public long fibN(long n) {
+		if(fibService==null) return -1;
+		try {
+			return fibService.fibN(n);
+		} catch (RemoteException e) {
+			e.printStackTrace();
+			return -1;
+		}
+	}
+
+}
