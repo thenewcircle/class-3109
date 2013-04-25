c82f3190b0c9264260a3d9257a3cd89d736d1abe
Marko Gargenta <marko@marakana.com>
2013-04-25 15:13:14 -0700

    Added dynamic permission enforcement

--- FibService/src/com/cisco/fibservice/IFibServiceImpl.java
+++ FibService/src/com/cisco/fibservice/IFibServiceImpl.java
@@ -1,5 +1,7 @@
 package com.cisco.fibservice;
 
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.RemoteException;
 
 import com.cisco.fibcommon.FibRequest;
@@ -7,22 +9,41 @@ import com.cisco.fibcommon.IFibListener;
 import com.cisco.fibcommon.IFibService;
 
 public class IFibServiceImpl extends IFibService.Stub {
+	private Context context;
+
+	public IFibServiceImpl(Context context) {
+		super();
+		this.context = context;
+	}
+
+	private static final int N_THRESHOLD = 35;
+
+	/** Permission check. */
+	private void checkPermissions(long n) {
+		if (n > N_THRESHOLD
+				&& context
+						.checkCallingOrSelfPermission("com.cisco.permission.FIB_SERVICE_HIGH_NUMBERS") != PackageManager.PERMISSION_GRANTED) {
+			throw new SecurityException("Not allowed for value of n: " + n);
+		}
+	}
 
 	@Override
 	public long fibJ(long n) throws RemoteException {
+		checkPermissions(n);
 		return FibLib.fibJ(n);
 	}
 
 	@Override
 	public long fibN(long n) throws RemoteException {
+		checkPermissions(n);
 		return FibLib.fibN(n);
 	}
 
 	@Override
 	public long fib(FibRequest request) throws RemoteException {
-		if( request.getAlgorithm()==FibRequest.ALGORITHM_JAVA)
+		if (request.getAlgorithm() == FibRequest.ALGORITHM_JAVA)
 			return fibJ(request.getN());
-		else if( request.getAlgorithm()==FibRequest.ALGORITHM_NATIVE)
+		else if (request.getAlgorithm() == FibRequest.ALGORITHM_NATIVE)
 			return fibN(request.getN());
 		else
 			return -1;
