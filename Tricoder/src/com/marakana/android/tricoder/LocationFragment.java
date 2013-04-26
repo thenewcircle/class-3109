83b3b385ffc11e53020b6ae7da7616da3c89bd11
Marko Gargenta <marko@marakana.com>
2013-04-26 13:05:46 -0700

    Added Geocoder

--- Tricoder/src/com/marakana/android/tricoder/LocationFragment.java
+++ Tricoder/src/com/marakana/android/tricoder/LocationFragment.java
@@ -1,7 +1,12 @@
 package com.marakana.android.tricoder;
 
+import java.io.IOException;
+import java.util.List;
+
 import android.app.Fragment;
 import android.content.Context;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -15,8 +20,9 @@ public class LocationFragment extends Fragment {
 	private static final String PROVIDER = LocationManager.GPS_PROVIDER;
 	private static final int MIN_TIME = 30000; // 30 seconds
 	private static final int MIN_DISTANCE = 10; // 10 meters
-	private static TextView textLat, textLong, textAlt;
+	private static TextView textLat, textLong, textAlt, textAddress;
 	private LocationManager locationManager;
+	private static Geocoder geocoder;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -25,6 +31,7 @@ public class LocationFragment extends Fragment {
 		textLat = (TextView) view.findViewById(R.id.text_lat);
 		textLong = (TextView) view.findViewById(R.id.text_long);
 		textAlt = (TextView) view.findViewById(R.id.text_alt);
+		textAddress = (TextView) view.findViewById(R.id.text_address);
 
 		return view;
 	}
@@ -35,21 +42,24 @@ public class LocationFragment extends Fragment {
 		locationManager = (LocationManager) getActivity().getSystemService(
 				Context.LOCATION_SERVICE);
 		Location location = locationManager.getLastKnownLocation(PROVIDER);
+		geocoder = new Geocoder(getActivity());
+
 		updateView(location);
 	}
 
 	@Override
 	public void onStart() {
 		super.onStart();
-		locationManager.requestLocationUpdates(PROVIDER, MIN_TIME, MIN_DISTANCE, LISTENER);
+		locationManager.requestLocationUpdates(PROVIDER, MIN_TIME,
+				MIN_DISTANCE, LISTENER);
 	}
-	
+
 	@Override
 	public void onStop() {
 		super.onStop();
 		locationManager.removeUpdates(LISTENER);
 	}
-	
+
 	private static final LocationListener LISTENER = new LocationListener() {
 
 		@Override
@@ -58,16 +68,18 @@ public class LocationFragment extends Fragment {
 		}
 
 		@Override
-		public void onProviderDisabled(String provider) {			
+		public void onProviderDisabled(String provider) {
 		}
+
 		@Override
-		public void onProviderEnabled(String provider) {			
+		public void onProviderEnabled(String provider) {
 		}
+
 		@Override
 		public void onStatusChanged(String provider, int status, Bundle extras) {
 		}
 	};
-	
+
 	private static void updateView(Location location) {
 		if (location == null) {
 			textLat.setText("Location unknown");
@@ -77,6 +89,24 @@ public class LocationFragment extends Fragment {
 			textLat.setText(String.format("%.3f", location.getLatitude()));
 			textLong.setText(String.format("%.3f", location.getLongitude()));
 			textAlt.setText(String.format("%.0f", location.getAltitude()));
+
+			// Geocode the location to address
+			textAddress.setText("");
+			try {
+				List<Address> addresses = geocoder.getFromLocation(
+						location.getLatitude(), location.getLongitude(), 1);
+				if (addresses == null || addresses.size() == 0) {
+					textAddress.setText("Unknown address");
+					return;
+				}
+				Address address = addresses.get(0);
+				for (int i = 0; i < address.getMaxAddressLineIndex(); i++) {
+					textAddress.append("\n"+address.getAddressLine(i));
+				}
+			} catch (IOException e) {
+				textAddress.setText("Unknown address");
+				e.printStackTrace();
+			}
 		}
 	}
 }
